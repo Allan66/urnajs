9c365a7747b1b2a95933c99de9b83c12ded01cba
Allan66 <allan.aurelio@hotmail.com>
2020-10-09 12:29:03 -0300

    simulacao de urna

---
+++ etapas.js
@@ -0,0 +1,47 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '45451',
+                nome: 'Chaves',
+                partido: 'PVILA',
+                fotos: [
+                    { url: '38111.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '71712',
+                nome: 'Madruguinha',
+                partido: 'PVILA',
+                fotos: [
+                    { url: 'madruga.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '66',
+                nome: 'Dollynho',
+                partido: 'PDM',
+                vice: 'Laranjo',
+                fotos: [
+                    { url: 'dollynho.jpg', legenda: 'Prefeito' },
+                    { url: 'laranjo.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84', // Exemplo de como adicionar outro candidato
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '84.jpg', legenda: 'Prefeito' },
+                    { url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
